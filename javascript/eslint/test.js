9a4feac7c04a730f68c3c016a89ba3f13f477707
hanshuai <hanshuai@baiwang.com>
2019-07-08 10:54:47 +0800

    add eslint

---
+++ javascript/eslint/test.js
@@ -0,0 +1,24 @@
+const func = () => {
+	var a
+	console.log(12, parseInt('111', 10), a)
+}
+
+func()
+
+function test () {
+	console.log('test')
+	alert(123)
+	let a = 1
+	switch (a) {
+		case 1:
+			console.log(1)
+			break
+		case 2:
+			console.log(2)
+			break
+		default:
+			console.log(3)
+	}
+}
+
+test()
